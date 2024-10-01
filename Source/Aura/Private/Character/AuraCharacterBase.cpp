2e4dc85d2a38108741f0f437832ad31fcfd4af98
Piotr Cyckiewicz <piotr.cyckiewicz.biz@gmail.com>
2024-10-01 19:46:40 +0200

    Reorganized AuraCharacterBase class
    Simplified and abstracted base character class, changed directory

---
+++ Source/Aura/Private/Character/AuraCharacterBase.cpp
@@ -0,0 +1,18 @@
+// Copyright C&D Games
+
+
+#include "Character/AuraCharacterBase.h"
+
+
+AAuraCharacterBase::AAuraCharacterBase()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+}
+
+void AAuraCharacterBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+

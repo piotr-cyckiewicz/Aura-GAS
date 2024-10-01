279165b4543099ceb89c93abb7461620459608b8
Piotr Cyckiewicz <piotr.cyckiewicz.biz@gmail.com>
2024-10-01 20:26:43 +0200

    Setup character blueprints
    - Added weapon mesh to base character class
    - Added hand sockets for Aura and goblin enemy skeletal meshes
    - Created basic blueprints for AuraCharacter and AuraEnemy

--- Source/Aura/Private/Character/AuraCharacterBase.cpp
+++ Source/Aura/Private/Character/AuraCharacterBase.cpp
@@ -8,6 +8,9 @@ AAuraCharacterBase::AAuraCharacterBase()
 {
 	PrimaryActorTick.bCanEverTick = false;
 
+	WeaponMesh = CreateDefaultSubobject<USkeletalMeshComponent>(FName("Weapon Mesh"));
+	WeaponMesh->SetupAttachment(GetMesh(), FName("WeaponHandSocket"));
+	WeaponMesh->SetCollisionEnabled(ECollisionEnabled::NoCollision);
 }
 
 void AAuraCharacterBase::BeginPlay()

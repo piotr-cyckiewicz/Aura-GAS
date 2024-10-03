865544c05a8e0aeb42f90c349f6ced86e46586ce
Piotr Cyckiewicz <piotr.cyckiewicz.biz@gmail.com>
2024-10-03 21:36:35 +0200

    Set up player's movement and top down view camera
    - Created and configured top down view camera
    - Configured player's rotation settings
    - Constrained player to plane
    - Improved animation blending between Idle and WalkRun for the player

--- Source/Aura/Private/Character/AuraCharacter.cpp
+++ Source/Aura/Private/Character/AuraCharacter.cpp
@@ -2,4 +2,34 @@
 
 
 #include "Character/AuraCharacter.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "Camera/CameraComponent.h"
 
+
+AAuraCharacter::AAuraCharacter()
+{
+	CameraSpringArm = CreateDefaultSubobject<USpringArmComponent>(FName("Camera Spring Arm"));
+	CameraSpringArm->SetupAttachment(RootComponent);
+
+	CameraSpringArm->SetRelativeRotation(FRotator(-45, 0, 0));
+	CameraSpringArm->bInheritPitch = false;
+	CameraSpringArm->bInheritRoll = false;
+	CameraSpringArm->bInheritYaw = false;
+	CameraSpringArm->TargetArmLength = 800.0f;
+	CameraSpringArm->bUsePawnControlRotation = false;
+
+	Camera = CreateDefaultSubobject<UCameraComponent>(FName("Camera"));
+	Camera->SetupAttachment(CameraSpringArm);
+	Camera->bUsePawnControlRotation = false;
+
+
+	GetCharacterMovement()->bOrientRotationToMovement = true;
+	GetCharacterMovement()->RotationRate = FRotator(0, 400, 0);
+	GetCharacterMovement()->bConstrainToPlane = true;
+	GetCharacterMovement()->bSnapToPlaneAtStart = true;
+
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationRoll = false;
+	bUseControllerRotationYaw = false;
+}
--- Source/Aura/Public/Character/AuraCharacter.h
+++ Source/Aura/Public/Character/AuraCharacter.h
@@ -6,12 +6,21 @@
 #include "Character/AuraCharacterBase.h"
 #include "AuraCharacter.generated.h"
 
-/**
- * 
- */
+class USpringArmComponent;
+class UCameraComponent;
+
 UCLASS()
 class AURA_API AAuraCharacter : public AAuraCharacterBase
 {
 	GENERATED_BODY()
 	
+public:
+	AAuraCharacter();
+
+protected:
+	UPROPERTY(EditAnywhere, Category = "Camera")
+	TObjectPtr<USpringArmComponent> CameraSpringArm;
+
+	UPROPERTY(EditAnywhere, Category = "Camera")
+	TObjectPtr<UCameraComponent> Camera;
 };

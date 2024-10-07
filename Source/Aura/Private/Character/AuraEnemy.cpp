a2f6bb149f6361bcea3238f5f29ce12c286e30ca
Piotr Cyckiewicz <piotr.cyckiewicz.biz@gmail.com>
2024-10-07 20:45:38 +0200

    Finished enemy highlighting mechanic
    - Configured custom depth pass on characters' meshes and weapons
    - Implemented toggling of custom depth rendering during highlight switch

--- Source/Aura/Private/Character/AuraEnemy.cpp
+++ Source/Aura/Private/Character/AuraEnemy.cpp
@@ -2,15 +2,32 @@
 
 
 #include "Character/AuraEnemy.h"
+#include "Aura/Aura.h"
 
 void AAuraEnemy::HighlightActor()
 {
+	GetMesh()->SetRenderCustomDepth(true);
+	WeaponMesh->SetRenderCustomDepth(true);
+
 	FString Log = GetActorNameOrLabel() + TEXT(" highlighted");
 	UE_LOG(LogTemp, Display, TEXT("%s"), * Log);
 }
 
 void AAuraEnemy::UnHighlightActor()
 {
+	GetMesh()->SetRenderCustomDepth(false);
+	WeaponMesh->SetRenderCustomDepth(false);
+
 	FString Log = GetActorNameOrLabel() + TEXT(" unhighlighted");
 	UE_LOG(LogTemp, Display, TEXT("%s"), *Log);
+}
+
+void AAuraEnemy::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// Set custom depth stencil value of the mesh to the same as in PP_Highlight material
+	// This, along with SetRenderCustomDepth(true) enables drawing outlines
+	GetMesh()->SetCustomDepthStencilValue(CUSTOM_DEPTH_RED);
+	WeaponMesh->SetCustomDepthStencilValue(CUSTOM_DEPTH_RED);
 }

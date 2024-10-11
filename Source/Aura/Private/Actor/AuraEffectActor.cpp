8ec1a7076c9656f6008f3ef39ca51cd1c7811b06
Piotr Cyckiewicz <piotr.cyckiewicz.biz@gmail.com>
2024-10-11 19:39:45 +0200

    Set up temporary code to test AttributeSet
    NOTE: This is a quick hack to test things, it will be scrapped soon

---
+++ Source/Aura/Private/Actor/AuraEffectActor.cpp
@@ -0,0 +1,43 @@
+// Copyright C&D Games
+
+
+#include "Actor/AuraEffectActor.h"
+#include "Components/SphereComponent.h"
+#include "AbilitySystemInterface.h"
+#include "AbilitySystemComponent.h"
+#include "AbilitySystem/AuraAttributeSet.h"
+
+AAuraEffectActor::AAuraEffectActor()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+	Mesh = CreateDefaultSubobject<UStaticMeshComponent>("Mesh");
+	SetRootComponent(Mesh);
+
+	Sphere = CreateDefaultSubobject<USphereComponent>("Sphere");
+	Sphere->SetupAttachment(RootComponent);
+}
+
+void AAuraEffectActor::OnOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	//TODO: Change this to apply a gameplay effect. rn I use ugly hack
+	if (IAbilitySystemInterface* ASActor = Cast<IAbilitySystemInterface>(OtherActor)) {
+		const UAuraAttributeSet* AuraAttributeSet = Cast<UAuraAttributeSet>(ASActor->GetAbilitySystemComponent()->GetAttributeSet(UAuraAttributeSet::StaticClass()));
+		UAuraAttributeSet* TestAttributeSet = const_cast<UAuraAttributeSet*>(AuraAttributeSet);
+		TestAttributeSet->SetHealth(AuraAttributeSet->GetHealth() + 25.0f);
+		Destroy();
+	}
+}
+
+void AAuraEffectActor::OnEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+
+}
+
+
+void AAuraEffectActor::BeginPlay()
+{
+	Super::BeginPlay();
+	Sphere->OnComponentBeginOverlap.AddDynamic(this, &AAuraEffectActor::OnOverlap);
+	Sphere->OnComponentEndOverlap.AddDynamic(this, &AAuraEffectActor::OnEndOverlap);
+}

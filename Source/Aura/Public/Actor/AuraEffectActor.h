4dec4aebd9f082dd329ea6118c9d5b8c41a20bbb
Piotr Cyckiewicz <piotr.cyckiewicz.biz@gmail.com>
2024-10-28 18:55:29 +0100

    Added BP_MaxHealthCrystal
    - Added DurtationGameplayEffectClass var in AuraEffectActor
    - Added duration gameplay effect CrystalHeal that increases max health
    - Added BP_MaxHealthCrystal that applies CrystalHeal effect to player
    - Placed Health Crystal in the level

--- Source/Aura/Public/Actor/AuraEffectActor.h
+++ Source/Aura/Public/Actor/AuraEffectActor.h
@@ -26,4 +26,7 @@ protected:
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Effects")
 	TSubclassOf<UGameplayEffect> InstantGameplayEffectClass;
 
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Effects")
+	TSubclassOf<UGameplayEffect> DurationGameplayEffectClass;
+
 };
